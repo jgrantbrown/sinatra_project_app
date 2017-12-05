173a3ad0292fc7766d060684119016e861cd28f9
JGBrown <j.grant.brown@gmail.com>
2017-12-05 10:42:40 -0500

    commnet update and destroy working

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -10,6 +10,24 @@ class CommentController < ApplicationController
     redirect "concerts/#{params[:concert_id]}"
   end
 
-  
+
+
+  delete '/comments/:id' do
+    Comment.find(params[:id]).destroy
+    redirect "concerts/#{params[:concert_id]}"
+  end
+
+get '/comments/:id/update' do
+  @comment=Comment.find(params[:id])
+  @concert= Concert.find(@comment.concert.id)
+
+  erb :'comments/update_comment'
+end
+
+put '/comments/:id' do
+  @comment=Comment.find(params[:id])
+  @comment.update(comment: params[:comment])
+  redirect "concerts/#{params[:concert_id]}"
+end
 
 end
